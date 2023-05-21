94c8b43be80e4828f5b5340a06f51fcabe3dc3f3
Jonah <jonahtkane@gmail.com>
2023-05-21 13:24:05 -0500

    edited seed.js file

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,22 +1,22 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User, Session } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const sessionSeeds = require('./sessionSeeds.json');
 
 db.once('open', async () => {
   try {
-    await Thought.deleteMany({});
+    await Session.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
+    for (let i = 0; i < sessionSeeds.length; i++) {
+      const { _id, sessionAuthor } = await Session.create(sessionSeeds[i]);
       const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
+        { username: sessionAuthor },
         {
           $addToSet: {
-            thoughts: _id,
+            sessions: _id,
           },
         }
       );
